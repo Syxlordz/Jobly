a339bdd80b54471ddd62b7f2dd153a4947aa509f
Syxlordz <144750539+Syxlordz@users.noreply.github.com>
2023-09-30 17:58:44 -0500

    Update templates.js

--- src/templates.js
+++ src/templates.js
@@ -19,7 +19,7 @@ export const jobTemplate = (job, currency) =>
       ? `Desde ${currency}${job.job_min_salary}`
       : '')}
   </h4>
-  <h4>Valoración de los candidatos que han aplicado:${job.job_apply_quality_score}/1</h4>
+  <h4>Valoración de los candidatos que han aplicado:${job.job_apply_quality_score.toString().substring(0, 3)}/1</h4>
   <h5>${job.job_city}</h5>
   <p class="card-text">${cortarDescripcion(job.job_description)}</p>
   <a class="Joblink" href="${job.job_apply_link}">View Job</a>
